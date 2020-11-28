ba9fcb17c17d913d1a678993bb9110e2dd3e5592
lucaperullo <72197904+lucaperullo@users.noreply.github.com>
2020-11-28 17:43:31 +0100

    a striveSchool project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import NavBar from "./components/NavBar";
+import FooterPlayer from "./components/FooterPlayer";
+import HomePage from "./components/HomePage";
+import ArtistPage from "./components/ArtistPage";
+import AlbumPage from "./components/AlbumPage";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    song: {},
+    image: "",
+  };
+  selectedSong = (selected, image) => {
+    this.setState({ song: selected });
+    this.setState({ image: image });
+  };
+  render() {
+    return (
+      <BrowserRouter>
+        <NavBar />
+        <Route path="/" exact component={HomePage} />
+        <Route
+          path="/artist/:id"
+          render={(props) => <ArtistPage {...props} />}
+        />
+        <Route
+          path="/album/:id"
+          render={(props) => (
+            <AlbumPage selectedSong={this.selectedSong} {...props} />
+          )}
+        />
+        <FooterPlayer song={this.state.song} image={this.state.image} />
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
